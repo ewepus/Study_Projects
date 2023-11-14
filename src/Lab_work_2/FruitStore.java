5b415fd6eeaeaf29b274f5930508bdc1ca0c47a9
ewepu <150716285+ewepus@users.noreply.github.com>
2023-11-14 12:18:05 +0300

    Update FruitStore.java

--- src/Lab_work_2/FruitStore.java
+++ src/Lab_work_2/FruitStore.java
@@ -10,22 +10,24 @@ public class FruitStore {
         double fruitWeight = Double.parseDouble(scanner.nextLine());
         double fruitPrice = 0.0;
         double discountedPrice = 0.0;
+        boolean errors = false;
         switch (fruitName) {
-            case "apple": fruitPrice += 9.99*23*fruitWeight; break;
-            case "plum": fruitPrice += 10.98*23*fruitWeight; break;
-            case "banana": fruitPrice += 11.49*23*fruitWeight; break;
-            case "grapes": fruitPrice += 15.99*23*fruitWeight; break;
-            case "orange": fruitPrice += 17.99*23*fruitWeight; break;
-            case "kiwi": fruitPrice += 37.99*23*fruitWeight; break;
-            case "mango": fruitPrice += 39.99*23*fruitWeight; break;
-            default: System.out.println("INVALID"); break;
+            case "apple" -> fruitPrice += 9.99*23*fruitWeight;
+            case "plum" -> fruitPrice += 10.98*23*fruitWeight;
+            case "banana" -> fruitPrice += 11.49*23*fruitWeight;
+            case "grapes" -> fruitPrice += 15.99*23*fruitWeight;
+            case "orange" -> fruitPrice += 17.99*23*fruitWeight;
+            case "kiwi" -> fruitPrice += 37.99*23*fruitWeight;
+            case "mango" -> fruitPrice += 39.99*23*fruitWeight;
+            default -> errors = true;
         }
         switch (dayName) {
-            case "monday","tuesday","wednesday","thursday","friday": discountedPrice += fruitPrice*0.95; break;
-            case "saturday","sunday": discountedPrice += 0; break;
-            default: System.out.println("INVALID"); break;
+            case "monday","tuesday","wednesday","thursday","friday" -> discountedPrice += fruitPrice*0.95;
+            case "saturday","sunday" -> discountedPrice += 0;
+            default-> errors = true;
         }
-        System.out.printf("%.2f",discountedPrice);
+        if (!errors) {System.out.printf("%.2f",discountedPrice);
     }
+        else System.out.println("INVALID");}
 }
 //var_5
